b9dc0fc4531d3efdec60f167afba4fecb1bfed2a
pusonja <pusonja@gmail.com>
2024-11-22 17:24:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<main>
+			<h1 className='text-5xl text-orange-500 font-bold'>
+				Welcome to DP-2024 site
+			</h1>
+		</main>
+	);
+};
+
+export default App;
