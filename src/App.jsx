5a4f1c94777df493e1ae40e3a50b4d4172f511cb
pusonja <pusonja@gmail.com>
2024-11-22 19:24:44 +0100

    20 00

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const App = () => {
 	return (
 		<main>
-			<h1 className='text-5xl text-orange-500 font-bold'>
-				Welcome to DP-2024 site
+			<h1 className='text-5xl text-violet-300 font-bold'>
+				Welcome to DP-2024
 			</h1>
 		</main>
 	);
